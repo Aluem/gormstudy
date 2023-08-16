6bc8ef1a8f10fe8067a92824bd0310bbc0dc0ceb
aluemhe <aluemhe@zego.im>
2023-08-16 23:19:51 +0800

    feat:声明式模型自定义,创建时间跟踪

--- declaringmodels_test.go
+++ declaringmodels_test.go
@@ -5,6 +5,18 @@ import (
 	"testing"
 )
 
+type DeclaringModelsTest struct {
+	Id        int
+	Name      string
+	CreatedAt int64
+	UpdatedAt int64
+	DeletedAt int64
+
+	Create int64 `gorm:"autoCreateTime"`
+	Update int64 `gorm:"autoUpdateTime"`
+	Delete int64 `gorm:"autoDeleteTime"`
+}
+
 func Test1(t *testing.T) {
 	db, err := GetBiReportDb()
 	if err != nil {
@@ -19,4 +31,19 @@ func Test1(t *testing.T) {
 
 	fmt.Println("success")
 	fmt.Println("id=", id)
+}
+
+func TestCreatingTracking(t *testing.T) {
+	db, err := GetBiReportDb()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = db.Debug().Table("declaring_models_test").Create(&DeclaringModelsTest{Name: "zhangsan"}).Error
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("success")
+
 }
